4a3e714547059aacd01c6d0d0cf9182627d273a3
jinjupeng <im.jp@outlook.com>
2020-08-14 16:35:18 +0800

    gRPC的简单实现

--- CoreNotes.gRPC.Client/CoreNotes.gRPC.Client/Program.cs
+++ CoreNotes.gRPC.Client/CoreNotes.gRPC.Client/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using CoreNotes.gRPC.Server.Protos;
+using Google.Protobuf;
 using Grpc.Core;
 using Grpc.Net.Client;
 
@@ -10,24 +13,141 @@ namespace CoreNotes.gRPC.Client
     {
         static async Task Main(string[] args)
         {
+            
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new EmployeeService.EmployeeServiceClient(channel);
+
+            var option = int.Parse(args[0]);
+            switch (option)
+            {
+                case 1:
+                    await GetByNoAsync(client);
+                    break;
+                case 2:
+                    await GetAllAsync(client);
+                    break;
+                case 3:
+                    await AddPhotoAsync(client);
+                    break;
+                case 5:
+                    await SaveAllAsync(client);
+                    break;
+            }
+
+            
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+
+        public static async Task GetByNoAsync(EmployeeService.EmployeeServiceClient client)
+        {
             var md = new Metadata
             {
                 {"username", "god"},
                 {"role", "administrator"}
             };
-
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new EmployeeService.EmployeeServiceClient(channel);
-
             var response = await client.GetByNoAsync(new GetByNoRequest
             {
                 No = 1994
             }, md);
 
             Console.WriteLine($"Response message: {response}");
-            Console.WriteLine("Press any key to exit");
+        }
 
-            Console.ReadKey();
+        public static async Task GetAllAsync(EmployeeService.EmployeeServiceClient client)
+        {
+            using var call = client.GetAll(new GetAllRequest());
+            var responseStream = call.ResponseStream;
+            while (await responseStream.MoveNext())
+            {
+                Console.WriteLine(responseStream.Current.Employee);
+            }
+        }
+
+        public static async Task AddPhotoAsync(EmployeeService.EmployeeServiceClient client)
+        {
+            var md = new Metadata
+            {
+                {"username", "god"},
+                {"role", "administrator"}
+            };
+
+            FileStream fs = File.OpenRead("logo.png");
+            using var call = client.AddPtoto(md);
+
+            var stream = call.RequestStream;
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int numRead = await fs.ReadAsync(buffer, 0, buffer.Length);
+                if (numRead == 0)
+                {
+                    break;
+                }
+
+                if (numRead < buffer.Length)
+                {
+                    Array.Resize(ref buffer, numRead);
+                }
+
+                await stream.WriteAsync(new AddPhotoRequest()
+                {
+                    Data = ByteString.CopyFrom(buffer)
+                });
+            }
+
+            await stream.CompleteAsync();
+
+            var response = await call.ResponseAsync;
+            Console.WriteLine(response.IsOk);
+        }
+
+        public static async Task SaveAllAsync(EmployeeService.EmployeeServiceClient client)
+        {
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    No = 111,
+                    FirstName = "Monica",
+                    LastName = "Geller",
+                    Salary = 7890.1f
+                },
+                new Employee
+                {
+                    No = 222,
+                    FirstName = "Joey",
+                    LastName = "Tri",
+                    Salary = 500
+                }
+            };
+
+            using var call = client.SaveAll();
+            var requestStream = call.RequestStream;
+            var responseStream = call.ResponseStream;
+
+            var responseTask = Task.Run(async () =>
+            {
+                while (await responseStream.MoveNext())
+                {
+                    Console.WriteLine($"Saved: {responseStream.Current.Employee}");
+                }
+            });
+
+            foreach (var employee in employees)
+            {
+                await requestStream.WriteAsync(new EmployeeRequest
+                {
+                    Employee = employee
+                });
+            }
+
+            await requestStream.CompleteAsync();
+            await responseTask;
+
         }
     }
 }
--- CoreNotes.gRPC.Server/CoreNotes.gRPC.Server/Services/MyEmployeeService.cs
+++ CoreNotes.gRPC.Server/CoreNotes.gRPC.Server/Services/MyEmployeeService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreNotes.gRPC.Server.Data;
@@ -37,6 +38,66 @@ namespace CoreNotes.gRPC.Server.Services
             }
 
             throw new Exception($"Employee not found with no: {request.No}");
+        }
+
+        public override async Task GetAll(GetAllRequest request, IServerStreamWriter<EmployeeResponse> responseStream, ServerCallContext context)
+        {
+            foreach (var employee in InMemoryData.Employees)
+            {
+                await responseStream.WriteAsync(new EmployeeResponse
+                {
+                    Employee = employee
+                });
+            }
+        }
+
+        public override async Task<AddPhotoResponse> AddPtoto(IAsyncStreamReader<AddPhotoRequest> requestStream, ServerCallContext context)
+        {
+            var md = context.RequestHeaders;
+            foreach (var pair in md)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+
+            var data = new List<byte>();
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine($"Received：{requestStream.Current.Data.Length} bytes");
+                data.AddRange(requestStream.Current.Data);
+            }
+
+            Console.WriteLine($"Received file with {data.Count} bytes");
+
+            return new AddPhotoResponse
+            {
+                IsOk = true
+            };
+        }
+
+        public override async Task SaveAll(IAsyncStreamReader<EmployeeRequest> requestStream, IServerStreamWriter<EmployeeResponse> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var employee = requestStream.Current.Employee;
+
+                // 并发锁
+                lock (employee)
+                {
+                    InMemoryData.Employees.Add(employee);
+                    employee.Id = new Random(1).Next();
+                }
+
+                await responseStream.WriteAsync(new EmployeeResponse
+                {
+                    Employee = employee
+                });
+            }
+
+            Console.WriteLine("Employee: ");
+            foreach (var employee in InMemoryData.Employees)
+            {
+                Console.WriteLine(employee);
+            }
         }
     }
 }
